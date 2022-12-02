f035f67124f9e1056bbe6ce0a0e2b80ed09c7277
Saygın <b.sayginnn@gmail.com>
2022-12-02 15:26:23 +0300

    Deserialization

--- Program.cs
+++ Program.cs
@@ -6,3 +6,7 @@ string rootPath = Environment.CurrentDirectory;
 string fullPath = Path.Combine(rootPath, folderName, fileName);
 
 string strJSON = File.ReadAllText(fullPath);
+
+var op = new JsonSerializerOptions();
+op.PropertyNameCaseInsensitive = false;
+var Movies = JsonSerializer.Deserialize<List<Movie>>(strJSON, op);
