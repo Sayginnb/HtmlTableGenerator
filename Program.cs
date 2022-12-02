50f965340d83cf085197d1fee01f0ebe4030eeaa
Saygın <b.sayginnn@gmail.com>
2022-12-02 15:37:03 +0300

    Write File

--- Program.cs
+++ Program.cs
@@ -10,3 +10,38 @@ string strJSON = File.ReadAllText(fullPath);
 var op = new JsonSerializerOptions();
 op.PropertyNameCaseInsensitive = false;
 var Movies = JsonSerializer.Deserialize<List<Movie>>(strJSON, op);
+
+string strHTML = "";
+strHTML += "<table>";
+
+strHTML += "<th>";
+strHTML += "<tr>";
+strHTML += "<td>id</td>";
+strHTML += "<td>title</td>";
+strHTML += "<td>rating</td>";
+strHTML += "<td>genre</td>";
+strHTML += "<td>duration</td>";
+strHTML += "</tr>";
+strHTML += "</th>";
+
+strHTML += "<tbody>";
+
+foreach (var item in Movies)
+{
+    strHTML += "<tr>";
+
+    strHTML += "<td>" + item.Id + "</td>";
+    strHTML += "<td>" + item.Title + "</td>";
+    strHTML += "<td>" + item.Rating + "</td>";
+    strHTML += "<td>" + item.Genre + "</td>";
+    strHTML += "<td>" + item.Duration + "</td>";
+
+    strHTML += "</tr>";
+}
+
+strHTML += "</tbody>";
+strHTML += "</table>";
+
+fileName = "output.html";
+fullPath = Path.Combine(rootPath,folderName,fileName);
+File.WriteAllText(fullPath, strHTML);
